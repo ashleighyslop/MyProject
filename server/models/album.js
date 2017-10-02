3c72943a845eff5e40babfa1e358f8b3dadc9e12
ashleighyslop <ahyslop2@sapient.com>
2017-10-02 09:54:56 +0100

    wrote my own API for album in the album.js file, changed album from a persisted model to a model in album.json file. Added more routing to root file

--- server/models/album.js
+++ server/models/album.js
@@ -1,5 +1,33 @@
 'use strict';
-
+//nested function within this function
 module.exports = function(Album) {
+  //callback = cb
+  Album.sayHello = function(message, req, cb) {
+    /*call back frist argument is the error, if not passing
+    any error put 'null'. Second argument is the result*/
+    console.log(req);
+    cb(null, message)
+  }
+  Album.remoteMethod('sayHello', {
+    accepts:[
+      {arg: 'message',
+      type: 'string',
+      http:{
+        source: 'query'
+      }},
+      {arg: 'req',
+      type: 'object',
+      http:{
+        source: 'req'
+      }}],
+    returns:{
+      arg: 'greeting',
+      type:'string'
+    },
+    http:{
+      path: '/hello',
+      verb: 'GET'
+    }
+  })
 
 };
